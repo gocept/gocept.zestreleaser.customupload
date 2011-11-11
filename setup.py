f02533a50caaa61a85fd48a7ed91763f71a54601
Wolfgang Schnerring <wosc@wosc.de>
2011-11-11 13:03:44 +0000

    Back to development: 1.0.4dev

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.zestreleaser.customupload',
-    version='1.0.3',
+    version='1.0.4dev',
     author='Wolfgang Schnerring',
     author_email='ws@gocept.com',
     url='',
