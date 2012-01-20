d1904d7d5ace718285c7a49fe31a2871cfb05d5f
Wolfgang Schnerring <ws@gocept.com>
2012-01-20 12:02:00 +0100

    Back to development: 1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.zestreleaser.customupload',
-    version='1.2',
+    version='1.3.dev0',
     author='Wolfgang Schnerring <ws at gocept dot com>, Christian Zagrodnick <cz at gocept dot com>',
     author_email='ws@gocept.com',
     url='https://code.gocept.com/svn/gocept/gocept.zestreleaser.customupload',
