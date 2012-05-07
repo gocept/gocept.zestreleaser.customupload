0348b938ea9e0ba1366e4df7fab9b7c0edf71a7a
Christian Zagrodnick <cz@flyingcircus.io>
2012-05-07 12:34:06 +0200

    Back to development: 1.2.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.zestreleaser.customupload',
-    version='1.2.1',
+    version='1.2.2.dev0',
     author='Wolfgang Schnerring <ws at gocept dot com>, Christian Zagrodnick <cz at gocept dot com>',
     author_email='ws@gocept.com',
     url='https://code.gocept.com/svn/gocept/gocept.zestreleaser.customupload',
